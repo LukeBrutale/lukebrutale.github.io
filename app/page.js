17eef27f032d750f24b0c544575d9fd3cd38dbdf
LukeBrutale <yuriybema@gmail.com>
2024-06-15 14:06:47 +0300

    commit

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
+import Index from "./components/homepage/index/Index";
 import ContactSection from "./components/homepage/contact";
 import Education from "./components/homepage/education";
 import Experience from "./components/homepage/experience";
@@ -26,6 +27,7 @@ export default async function Home() {
 
   return (
     <>
+      <Index />
       <HeroSection />
       <AboutSection />
       <Experience />
